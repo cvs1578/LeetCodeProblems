e079b33d8c501eaea1d6af863b97f9523b459c19
cvs1578 <charlievscola@gmail.com>
2022-08-10 18:06:37 -0500

    Added Comment

--- TwoSum.py
+++ TwoSum.py
@@ -11,4 +11,4 @@ def twoSum(nums, target):
         else: #If the number isn't in hte list, then we will add i to the list
             numbersLookedAt[nums[i]] = i
 
-print (twoSum([2,7,11,15],9))
+print (twoSum([2,7,11,15],9)) #This is the example problem
