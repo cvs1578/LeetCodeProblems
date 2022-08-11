cfb0ef08be03c3d8bd85c9869c6403c92b068c21
cvs1578 <charlievscola@gmail.com>
2022-08-10 19:00:42 -0500

    Problem 2: Added Comments

--- addTwoNumbers.py
+++ addTwoNumbers.py
@@ -1,7 +1,7 @@
 #Problem 2: Given two linked list with digits stored in reverse order and each digit containing a single list
 #Add the two numbers and return the sum as a linked list.
 #Definition for singly-linked list.
-class ListNode(object):
+class ListNode(object): #This is the Linked list def in LeetCode
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
@@ -12,23 +12,25 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
-        head = ListNode()
-        temp = head
-        carry = 0
-        while l1 and l2:
-            temp.val = l1.val + l2.val + carry
-            if temp.val>=10:
+        head = ListNode() #Answer will be a linked list, so first create a list
+        temp = head #temp is going through the list
+        carry = 0 #Carry because if the number is greater than 9 then we have to add 1
+        while l1 and l2: #Go through each list
+            temp.val = l1.val + l2.val + carry #Value will be first list + second + carry
+            if temp.val>=10: #Determine if we have the carry
                 temp.val -=10
                 carry=1
             else:
                 carry = 0
-            l1 = l1.next
+            #Next we will go through add to the linked list
+            l1 = l1.next 
             l2 = l2.next
+            #This will determine if the answer link list is appended
             if l1 or l2 or carry == 1:
                 temp.next = ListNode()
                 temp = temp.next
-        while l1:
-            temp.val = l1.val+carry
+        while l1: #Only L1 has values
+            temp.val = l1.val+carry 
             if temp.val>=10:
                 temp.val -=10
                 carry=1
@@ -38,7 +40,7 @@ class Solution(object):
             if l1 or carry == 1:
                 temp.next = ListNode()
                 temp = temp.next
-        while l2:
+        while l2: #Only L2 has values
             temp.val = l2.val+carry
             if temp.val>=10:
                 temp.val -=10
@@ -49,6 +51,6 @@ class Solution(object):
             if l2 or carry==1:
                 temp.next = ListNode()
                 temp = temp.next
-        if carry == 1:
+        if carry == 1: #See if there is a carry, then we just plop a 1 at the end
             temp.val = 1
-        return head
+        return head #Since we didn't move the head of the answer, we just return the head.
