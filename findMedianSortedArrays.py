ea2c0f0bb3a814f6ad6515ae28577f88e17ad659
cvs1578 <charlievscola@gmail.com>
2022-08-12 22:45:11 -0500

    Still not working

--- findMedianSortedArrays.py
+++ findMedianSortedArrays.py
@@ -12,7 +12,13 @@ def findMedianSortedArrays(nums1, nums2):
     #plan: Two pointers going in order from both nums. 
     #Basically create a sorted array from the two and stop when we hit the middle. 
     #Pretty sure the complexity would be O((n+m)/2) though and not log(m+n), so this probably will not work.
-    length = int(len(nums1)+len(nums2)/2)
+    totalLen = len(nums1)+len(nums2)
+    Even = False 
+    length = int(totalLen/2)
+    if totalLen%2 == 0:
+        Even = True
+        length +=1
+
     point1 = 0
     point2 = 0
     sortedarray = []
@@ -20,7 +26,18 @@ def findMedianSortedArrays(nums1, nums2):
         if point1<len(nums1) and point2<len(nums2):
             if nums1[point1]<nums2[point2]:
                 sortedarray.append(nums1[point1])
-                nums1+=1
+                point1+=1
             else:
-                sortedarray.append(nums2(point2))
-                nums2+=1
+                sortedarray.append(nums2[point2])
+                point2+=1
+        elif point1<len(nums1):
+            sortedarray.append(nums1[point1])
+            point1+=1
+        else:
+            sortedarray.append(nums2[point2])
+            point2+=1
+    if Even:
+        sortedarray[-1] = (sortedarray[-2]+sortedarray[-1])/2
+    return sortedarray[-1]
+
+print(findMedianSortedArrays([1,2],[3,4]))
